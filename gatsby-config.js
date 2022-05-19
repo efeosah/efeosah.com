b877ec30e3f5e944bfc55ff972d4494b403f3dab
efe osah <onanefeosah@icloud.com>
2022-05-19 14:57:11 -0400

    First Commit

---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    title: `efeosah.com`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-react-helmet", "gatsby-plugin-sitemap"]
+};

5b7dbdbc1fabc6d03ef0814e659af28c324b94f7
efe osah <onanefeosah@icloud.com>
2022-06-10 09:15:20 -0400

    Update projects.js

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -39,6 +39,17 @@ const ProjectList = [
                     The API is restful and makes use of a MERN stack for development
                   `,
     },
+    {
+        name: "Spotify Clone//miscdisc ",
+        tagline: "A spotify clone web app with a focus on song recommendations to the user",
+        image: "https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_CMYK_Black.png",
+        url: null,
+        description: `
+                    Clone app project I made using spotifys API. The main reason for the project was to create a web app 
+                    that recommended new songs for me based on my listening history, but I stretched it out to be 
+                    a clone-like Spotify app instead.
+                  `,
+    },
     //Add Misc projects
 ];
 export default function Project({ data }) {
